e7dad625016a4667d1cc47c8806ec70809b22344
Aditya Mahajan <aditya1612004@gmail.com>
2023-11-25 10:37:07 +0530

    Implement edit feature

--- index.js
+++ index.js
@@ -31,6 +31,21 @@ app.post("/delete",(req,res)=>{
   blogs.splice(index,1);
   res.redirect("/");
 })
+let index2;
+app.post("/edit",(req,res)=>{
+  var index = req.body.edit;
+  index2=index;
+  res.render("edit.ejs",{
+    names: names,
+    index: index,
+    blogs: blogs
+  })
+})
+app.post("/edited",(req,res)=>{
+  names[index2]=req.body.Name;
+  blogs[index2]=req.body.Content;
+  res.redirect("/");
+})
 
 app.listen(port,()=>{
     console.log(`Server running at port ${port}`);
